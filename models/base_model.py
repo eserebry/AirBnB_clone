60b91673c589bad4c5ee2886903c2c20737abeb3
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-06-08 23:53:58 +0000

    updated return value for dictionary as well as public instab=nce attributes

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ defines all common attributes/methods for other classes """
 import uuid
 import datetime
+import json
 
 
 class BaseModel:
@@ -13,22 +14,9 @@ class BaseModel:
             created_at (int): datetimr
             updated_at (int): datetime
         """
-        self.id = None
-        self.created_at = None
-        self.updated_at = None
-
-    def id(self):
-        """assign with an uuid when an instance is created"""
-        id = uuid.uuid4()
-        return str(self.id)
-
-    def created_at(self):
-        """assign with the current datetime when an instance is created"""
-        return self.created_at
-
-    def updated_at(self):
-        """ assign with the current datetime when an instance is created"""
-        return self.updated_at
+        self.id = str(uuid.uuid4())
+        self.created_at = str(datetime.datetime.now())
+        self.updated_at = str(datetime.datetime.now())
 
     def save(self):
         """updates the public attribute updated_at with the current datetime"""
@@ -36,8 +24,12 @@ class BaseModel:
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
+        return {'__class__': self.__class__.__name__,
+                'created_at': self.created_at,
+                'id': self.id,
+                'updated_at': self.updated_at}
 
     def __str__(self):
         """returns string with the BaseModel arguments"""
-        return "[BaseModel] (%s) %s" % \
-            (self.id, self.__dict__)
+        return "[%s] (%s) %s" % \
+            (self.__class__.__name__, self.id, self.__dict__)
