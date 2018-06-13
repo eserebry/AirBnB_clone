e50735ecc95b0b2fd2c577969b04bef74b22aeec
Andrew Keil <monkeyninja147@gmail.com>
2018-06-13 05:52:30 +0000

    Updating basemodel with storage variables

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 import datetime
 import json
+from models import storage
 
 
 class BaseModel:
@@ -24,10 +25,12 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            storage.new(self)
 
     def save(self):
         """updates the public attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__"""
