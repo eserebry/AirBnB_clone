ba73c743e502e4443f623774dc6275a7d37ced0c
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-06-13 03:09:15 +0000

    dict representation updated

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,11 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at':
-                    setattr(self, 'created_at', '%Y-%m-%dT%H:%M:%S.%f')
-                if key == 'updated_at':
-                    setattr(self, 'created_at', '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.datetime.strptime
+                        (value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
@@ -34,7 +35,7 @@ class BaseModel:
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = my_dict['created_at'].isoformat()
         my_dict['id'] = self.id
-        my_dict['updated_at'] = self.updated_at
+        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
         return my_dict
 
     def __str__(self):
