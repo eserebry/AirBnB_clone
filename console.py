4a96cbdc9ee6a3453f3ac8c0b8b7422ddd1a27f9
Andrew Keil <monkeyninja147@gmail.com>
2018-06-13 01:08:28 +0000

    Final class handling console

--- console.py
+++ console.py
@@ -15,12 +15,14 @@ from models import storage
 from datetime import datetime
 
 
-
 class HBNBCommand(cmd.Cmd):
     """
     Class HBNBCommand processor
     """
     prompt = ' (hbnb) '
+
+    if __name__ == '__main__':
+        HBNBCommand().cmdloop()
 
     def do_EOF(self, line):
         """
@@ -191,5 +193,149 @@ class HBNBCommand(cmd.Cmd):
                 else:
                     print("** class doesn't exist **")
 
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+    def create_updatestr(line):
+        """
+        Method create string for update method using class name
+        """
+        args = line.split()
+        args0 = args[0].replace('"', "")
+        tmp = args0.replace(',', "")
+        attribute = args[1].replace('"', "")
+        attribute = attribute.replace(',', "")
+        value = args[2].replace('"', "")
+        value = value.replace(',', "")
+        returnvalue = tmp + " " + attribute + " " + value
+        return returnvalue
+
+    def do_BaseModel(self, line):
+        """
+        Method handling BaseModel
+        """
+        if line == '.all()':
+            self.do_all("BaseModel")
+        elif line == '.count()':
+            print(self.get_instanceCount("BaseModel"))
+        elif line[0:5] == '.show':
+            argument = 'BaseModel' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "BaseModel" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "BaseModel" + " " + rtrn
+            self.do_update(argument)
+
+    def do_User(self, line):
+        """
+        Method handling User
+        """
+        if line == '.all()':
+            self.do_all("User")
+        elif line == '.count()':
+            print(self.get_instanceCount("User"))
+        elif line[0:5] == '.show':
+            argument = 'User' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "User" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "User" + " " + rtrn
+            self.do_update(argument)
+
+    def do_City(self, line):
+        """
+        Method handling City
+        """
+        if line == '.all()':
+            self.do_all("City")
+        elif line == '.count()':
+            print(self.get_instanceCount("City"))
+        elif line[0:5] == '.show':
+            argument = 'City' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "City" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "City" + " " + rtrn
+            self.do_update(argument)
+
+    def do_State(self, line):
+        """
+        Method handling State
+        """
+        if line == '.all()':
+            self.do_all("State")
+        elif line == '.count()':
+            print(self.get_instanceCount("State"))
+        elif line[0:5] == '.show':
+            argument = 'State' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "State" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "State" + " " + rtrn
+            self.do_update(argument)
+
+    def do_Place(self, line):
+        """
+        Method handling Place
+        """
+        if line == '.all()':
+            self.do_all("Place")
+        elif line == '.count()':
+            print(self.get_instanceCount("Place"))
+        elif line[0:5] == '.show':
+            argument = 'Place' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "Place" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "Place" + " " + rtrn
+            self.do_update(argument)
+
+    def do_Amenity(self, line):
+        """
+        Method handling Amenity
+        """
+        if line == '.all()':
+            self.do_all("Amenity")
+        elif line == '.count()':
+            print(self.get_instanceCount("Amenity"))
+        elif line[0:5] == '.show':
+            argument = 'Amenity' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "Amenity" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "Amenity" + " " + rtrn
+            self.do_update(argument)
+
+    def do_Review(self, line):
+        """
+        Method handling Reviews
+        """
+        if line == '.all()':
+            self.do_all("Review")
+        elif line == '.count()':
+            print(self.get_instanceCount("Review"))
+        elif line[0:5] == '.show':
+            argument = 'Review' + " " + line[7:-2]
+            self.do_show(argument)
+        elif line[0:8] == '.destroy':
+            argument = "Review" + " " + line[10:-2]
+            self.do_destroy(argument)
+        elif line[0:7] == '.update':
+            rtrn = build_updatearg(line[8:-1])
+            argument = "Review" + " " + rtrn
+            self.do_update(argument)
