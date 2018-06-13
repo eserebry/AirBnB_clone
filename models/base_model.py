8274187767336ba94e1d7168e3b0b29942b79e1e
Elena Serebryakova <elenaserebryakoff@gmail.com>
2018-06-13 00:19:06 +0000

    re-creating an instance with the dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -7,16 +7,22 @@ import json
 
 class BaseModel:
     """defines all common attributes/methods for other classes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialise class BaseMode
         Args:
-            id (str): uuid
-            created_at (int): datetime
-            updated_at (int): datetime
+            *args - not used
+            **kwargs - dictionary, contains arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.now())
-        self.updated_at = str(datetime.datetime.now())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    setattr(self, 'created_at', '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'updated_at':
+                    setattr(self, 'created_at', '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def save(self):
         """updates the public attribute updated_at with the current datetime"""
@@ -26,7 +32,7 @@ class BaseModel:
         """returns a dictionary containing all keys/values of __dict__"""
         my_dict = self.__dict__
         my_dict['__class__'] = self.__class__.__name__
-        my_dict['created_at'] = self.created_at
+        my_dict['created_at'] = my_dict['created_at'].isoformat()
         my_dict['id'] = self.id
         my_dict['updated_at'] = self.updated_at
         return my_dict
