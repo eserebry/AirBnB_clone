2e3dfd287d9ca177e5bae792de3e816e1d795452
Andrew Keil <monkeyninja147@gmail.com>
2018-06-14 05:05:30 +0000

    modified base_model unit tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,16 +11,33 @@ class TestBaseModel(unittest.TestCase):
         """instance setup"""
         self.basemodel = BaseModel()
 
-    def test_class(self):
-        """testing for a type of the attributes and
-        if attributes are not empty"""
-        model = BaseModel()
-        self.assertIsNotNone(model.id)
-        self.assertIsNotNone(model.created_at)
-        self.assertIsNotNone(model.updated_at)
-        self.assertIsInstance(model.id, str)
-        self.assertIsInstance(model.created_at, datetime.datetime)
-        self.assertIsInstance(model.updated_at, datetime.datetime)
+    def testattr(self):
+        """Testing the attributes of BaseModel"""
+        self.assertTrue(hasattr(self.basemodel, "created_at"))
+        self.assertTrue(hasattr(self.basemodel, "id"))
+        self.assertFalse(hasattr(self.basemodel, "updated_at"))
+        self.assertFalse(hasattr(self.basemodel, "random_attr"))
+        self.assertFalse(hasattr(self.basemodel, "name"))
+        self.basemodel.name = "Betty"
+        self.basemodel.age = 89
+        self.assertTrue(hasattr(self.basemodel, "name"))
+        self.assertEqual(self.basemodel.name, "Betty")
+        self.assertTrue(hasattr(self.basemodel, "age"))
+        delattr(self.basemodel, "name")
+        self.assertFalse(hasattr(self.basemodel, "name"))
+        self.assertEqual(self.basemodel.__class__.__name__, "BaseModel")
+
+    def testmethod(self):
+        """Testing the methods of BaseModel"""
+        self.basemodel.save()
+        self.assertTrue(hasattr(self.basemodel, "updated_at"))
+
+    def teststr(self):
+        """Testing the str format of BaseModel"""
+        s = "[{}] ({}) {}".format(self.basemodel.__class__.__name__,
+                                  str(self.basemodel.id),
+                                  self.basemodel.__dict__)
+        self.assertEqual(print(s), print(self.basemodel))
 
 if __name__ == '__main__':
     unittest.main()
