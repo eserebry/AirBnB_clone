7ed1a066e545d2dd98c5547d3acf0d33313c485c
Andrew Keil <monkeyninja147@gmail.com>
2018-06-14 05:11:13 +0000

    amenity unit test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -11,17 +11,27 @@ class TestAmenity(unittest.TestCase):
         """test setup"""
         self.amenity = Amenity()
 
-    def test_amenity(self):
-        """testing for a type of the attributes and
-        if attributes are not empty"""
-        amenity = Amenity()
-        self.assertIsNotNone(amenity.id)
-        self.assertIsNotNone(amenity.created_at)
-        self.assertIsNotNone(amenity.updated_at)
-        self.assertIsInstance(amenity.id, str)
-        self.assertIsInstance(amenity.created_at, datetime.datetime)
-        self.assertIsInstance(amenity.updated_at, datetime.datetime)
-        self.assertIsNotNone(amenity.name)
+    def testattr(self):
+        """Testing the attributes of Amenity"""
+        self.assertTrue(hasattr(self.amenity, "created_at"))
+        self.assertTrue(hasattr(self.amenity, "id"))
+        self.assertFalse(hasattr(self.amenity, "updated_at"))
+        self.assertFalse(hasattr(self.amenity, "random_attr"))
+        self.assertTrue(hasattr(self.amenity, "name"))
+        self.assertEqual(self.amenity.__class__.__name__, "Amenity")
+        self.assertEqual(self.amenity.name, "")
+
+    def testmethod(self):
+        """Testing the methods of Amenity"""
+        self.amenity.save()
+        self.assertTrue(hasattr(self.amenity, "updated_at"))
+
+    def teststr(self):
+        """Testing the str format of Amenity"""
+        s = "[{}] ({}) {}".format(self.amenity.__class__.__name__,
+                                  str(self.amenity.id),
+                                  self.amenity.__dict__)
+        self.assertEqual(print(s), print(self.amenity))
 
 if __name__ == '__main__':
     unittest.main()
